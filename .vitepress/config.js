57a73a7613a6904afce65f6d5c5327a365b4fab7
chunshan <1484082125@qq.com>
2022-11-08 21:30:10 +0800

    博客形式写内容

---
+++ .vitepress/config.js
@@ -0,0 +1,41 @@
+const { getPosts, generatePaginationPages } = require('./theme/serverUtils')
+
+async function config() {
+    const pageSize = 3
+    await generatePaginationPages(pageSize)
+    return {
+
+        title: 'blog',
+        base: '/',
+        description: '前端,博客,javascript,es6,vue,react',
+        ignoreDeadLinks: true,
+        themeConfig: {
+            posts: await getPosts(),
+            pageSize: pageSize,
+            website: 'https://github.com/chunshand', //copyright link
+            nav: [
+                { text: 'Home', link: '/' },
+                { text: 'Archives', link: '/pages/archives' },
+                { text: 'Tags', link: '/pages/tags' },
+                { text: 'About', link: '/pages/about' }
+                // { text: 'Airene', link: 'http://airene.net' }  -- External link test
+            ],
+            //outline:[2,3],
+            outlineTitle: '文章摘要',
+            socialLinks: [{ icon: 'github', link: 'https://github.com/chunshand' }],
+        },
+        srcExclude: ['README.md'], // exclude the README.md , needn't to compiler
+
+        vite: {
+            //build: { minify: false }
+            server: { port: 5000 },
+        },
+        /*
+      optimizeDeps: {
+          keepNames: true
+      }
+      */
+    }
+}
+
+module.exports = config()
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    purge: ['./docs/**/*.{vue,js,ts,jsx,tsx}',
-        './docs/.vitepress/**/*.{vue,js,ts,jsx,tsx}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
